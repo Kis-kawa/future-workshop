b15a978e01334f81df0b62756ee36dd46a8bc469
Kyohei Kisikawa <m21041@g.metro-cit.ac.jp>
2024-05-22 12:13:59 +0900

    add(kis): 一番外の大枠

--- judotimer/lib/page_b.dart
+++ judotimer/lib/page_b.dart
@@ -12,13 +12,115 @@ class PracticeSetting extends StatelessWidget {
   );
 
   final appBar = AppBar(
-    title: const Text("練習モードの設定"),
-    backgroundColor: Colors.green[300],
+    backgroundColor: Colors.transparent,
+    elevation: 0,
   );
 
+final backRegion = SizedBox(
+  child: AspectRatio(
+    aspectRatio: 1248 / 843,
+    child: Container(
+      padding: const EdgeInsets.all(30),
+      child: Container(
+        color: Colors.amber,//背景
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(10, 20, 5, 20),
+                child: Container(color: Colors.blue,),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                child: Container(color: Colors.red,),
+              ),
+            ),
+          ],
+        ),
+        ),
+    ),
+  ),
+);
+
+  // final backRegion = SizedBox(
+  //     child: Column(
+  //         children: [
+  //           Expanded(
+  //             flex: 9,
+  //             child: Container(
+  //               color: const Color.fromARGB(255, 24, 24, 24),
+  //               padding: const EdgeInsets.fromLTRB(30, 5, 30, 30),
+  //               child: Align(
+  //                 child: Container(
+  //                     color: Colors.amber,
+  //                     child: AspectRatio(
+  //                       aspectRatio: 1721 / 940,
+  //                       child: Column(
+  //                         children: [
+  //                           Expanded(
+  //                             flex: 9,
+  //                             child: Align(
+  //                               alignment: Alignment.center,
+  //                               child:Expanded(
+  //                                 child: Row(
+  //                                   children: [
+  //                                     Expanded(flex:4 ,child: Container(color: Colors.red,),),
+  //                                     Expanded(flex:6 ,child: Container(color: Colors.amber,),),
+  //                                     Expanded(flex:4 ,child: Container(color: Colors.red,),),
+  //                                   ],
+  //                                 ),
+  //                                 ),
+  //                             ),
+  //                           ),
+  //                           Expanded(
+  //                             flex: 1,
+  //                             child: Container(color: Colors.green),
+  //                           ),
+  //                           Expanded(
+  //                             flex: 9,
+  //                             child: Row(
+  //                               children: [
+  //                                 Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
+  //                                 Expanded(flex: 1, child: Container(color: Colors.amber,),),
+  //                                 Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
+  //                                 Expanded(flex: 1, child: Container(color: Colors.amber,),),
+  //                                 Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
+  //                               ],
+  //                             ),
+  //                           ),
+  //                         ],
+  //                       ),
+  //                     ),
+  //                   ),
+  //               ),
+  //             ),
+  //           ),
+  //           Expanded(
+  //               flex: 1,
+  //               child: Container(
+  //                 color: const Color.fromARGB(255, 76, 86, 87),
+  //                 child: Align(
+  //                   alignment: Alignment.center,
+  //                   child: Container(
+  //                     color: Colors.amber,
+  //                     width: 200,
+  //                     child: const Center(child: Text('ボタン')),
+  //                   ),
+  //                 ),
+  //               ),
+  //             ),
+  //         ],
+  //       ),
+  //     );
+
   final scaffold = Scaffold(
+    backgroundColor: const Color.fromARGB(255, 24, 24, 24),
     appBar: appBar,
-    body: const Text("練習: 設定"),
+    body: Center(child: backRegion,),
     drawer: drawer,
   );
 
