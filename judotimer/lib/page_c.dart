f0cd919c342a876b1f960a3a46cc9b73e225c2f2
Kyohei Kisikawa <m21041@g.metro-cit.ac.jp>
2024-07-11 20:41:39 +0900

    配置

--- judotimer/lib/page_c.dart
+++ judotimer/lib/page_c.dart
@@ -16,9 +16,133 @@ class GameHome extends StatelessWidget {
     elevation: 0,
   );
 
+final backRegion = SizedBox(
+      child: Column(
+          children: [
+            Expanded(
+              flex: 9,
+              child: Container(
+                color: const Color.fromARGB(255, 24, 24, 24),
+                padding: const EdgeInsets.fromLTRB(30, 5, 30, 30),
+                child: Align(
+                  child: Container(
+                      color: Colors.amber,
+                      child: AspectRatio(
+                        aspectRatio: 1721 / 940,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 9,
+                              child: Align(
+                                alignment: Alignment.center,
+                                child:Expanded(
+                                  child:Row(
+                                    children: [
+                                      Expanded(flex:4 ,child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Expanded(flex:1 ,child: Container(color: Colors.red,),),
+                                          Expanded(flex:2 ,child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                              Expanded(flex:1 ,child: Container(color: Colors.amber,),),
+                                              Expanded(flex:1 ,child: Column(children: [
+                                                Expanded(flex:1 ,child: Container(color: Colors.blueGrey,),),
+                                                Expanded(flex:1 ,child: Container(color: Colors.red,),),
+                                              ],),),
+                                            ],
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                      Expanded(flex:10 ,child: Container(color: Colors.amber,),),
+                                      Expanded(flex:4 ,child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Expanded(flex:1 ,child: Container(color: Colors.red,),),
+                                          Expanded(flex:2 ,child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                              Expanded(flex:1 ,child: Column(children: [
+                                                Expanded(flex:1 ,child: Container(color: Colors.blueGrey,),),
+                                                Expanded(flex:1 ,child: Container(color: Colors.red,),),
+                                              ],),),
+                                              Expanded(flex:1 ,child: Container(color: Colors.amber,),),
+                                            ],
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                    ],
+                                  ),
+                                  ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children: [
+                                Expanded(flex: 1, child: Container(color: Colors.black,),),
+                                Expanded(flex: 10, child: Container(color: Colors.white,),),
+                                Expanded(flex: 18, child: Container(color: Colors.black,),),
+                                Expanded(flex: 10, child: Container(color: Colors.blueAccent,),),
+                                Expanded(flex: 1, child: Container(color: Colors.black,),),
+                              ],),
+                            ),
+                            Expanded(
+                              flex: 9,
+                              child: Row(
+                                children: [
+                                  Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
+                                  Expanded(flex: 1, child: Container(color: Colors.amber,),),
+                                  Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
+                                  Expanded(flex: 1, child: Container(color: Colors.amber,),),
+                                  Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 1,
+                child: Container(
+                  color: const Color.fromARGB(255, 76, 86, 87),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(flex: 3, child: Row(children: [
+                          Expanded(flex: 4, child: Container(color: Colors.blueAccent,),),
+                          Expanded(flex: 2, child: Container(color: Colors.green,),),
+                          Expanded(flex: 3, child: Container(color: Colors.blueAccent,),),
+                        ],),),
+                        Expanded(flex: 2, child: Container(color: Colors.green,),),
+                        Expanded(flex: 3, child: Container(color: Colors.blueAccent,),),
+                        Expanded(flex: 2, child: Container(color: Colors.green,),),
+                        Expanded(flex: 3, child: Row(children: [
+                          Expanded(flex: 3, child: Container(color: Colors.blueAccent,),),
+                          Expanded(flex: 2, child: Container(color: Colors.green,),),
+                          Expanded(flex: 4, child: Container(color: Colors.blueAccent,),),
+                        ],),),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      );
+
+
   final scaffold = Scaffold(
+    backgroundColor: const Color.fromARGB(255, 24, 24, 24),
     appBar: appBar,
-    body: const Text("試合: ホーム"),
+    body: backRegion,
     drawer: drawer,
   );
 
