6e608b0bfd79e8ff2ba998153415d0a5798f5a21
Kyohei Kisikawa <m21041@g.metro-cit.ac.jp>
2024-07-12 19:02:58 +0900

    add(kis): 背景

--- judotimer/lib/page_d.dart
+++ judotimer/lib/page_d.dart
@@ -1,11 +1,71 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:judotimer/drawer.dart';
+
+
+class PlayerColor extends StatelessWidget {
+  final int numcolor;
+  const PlayerColor({
+    super.key,
+    required this.numcolor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    "$numcolor";
+
+    Color containerColor;
+
+    if (numcolor == 0) {
+      containerColor = Colors.white;
+    } else if (numcolor == 1) {
+      containerColor = Colors.red;
+    } else if (numcolor == 2) {
+      containerColor = Colors.blue;
+    } else {
+      containerColor = Colors.black; // 決めてない
+    }
+
+    return Container(
+      color: containerColor,
+      padding: const EdgeInsets.all(5),
+    );
+  }
+}
+
 
 class GameSetting extends StatelessWidget {
   const GameSetting({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+  final RightContant = Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                    PlayerColor(numcolor: 2),
+                    PlayerColor(numcolor: 0),
+                  ],);
+
+  final LiteContantu = Container(color: Colors.amber,);
+
+  final con = Align(
+    alignment: Alignment.center,
+    child: Container(
+      margin: const EdgeInsets.fromLTRB(100, 40, 100, 40),
+      color: Colors.red,
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(40, 40, 40, 40),
+          color: Colors.amber,
+          child: Container(color: Colors.green, child: Row(
+            children: [
+              Expanded(flex: 1, child: Container(child: RightContant),),
+              Expanded(flex: 3, child: Container(child: LiteContantu),),
+            ],
+          ),),
+        ),
+      ),
+  );
+
 
   const drawer = Drawer(
     child: SideBar(),
@@ -18,7 +78,7 @@ class GameSetting extends StatelessWidget {
 
   final scaffold = Scaffold(
     appBar: appBar,
-    body: const Text("試合: 設定"),
+    body: con,
     drawer: drawer,
   );
 
