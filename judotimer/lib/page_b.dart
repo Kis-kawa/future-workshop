6986df35475fde08990322d3820719b3d90be07b
Kyohei Kisikawa <m21041@g.metro-cit.ac.jp>
2024-08-01 18:45:27 +0900

    kis(add):bのappbar

--- judotimer/lib/page_b.dart
+++ judotimer/lib/page_b.dart
@@ -12,7 +12,31 @@ class PracticeSetting extends StatelessWidget {
   );
 
   final appBar = AppBar(
-    backgroundColor: Colors.transparent,
+    backgroundColor: const Color.fromARGB(255, 34, 130, 135),
+    title: const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Spacer(),
+        SizedBox(width: 150),
+        Text(
+          "練習の設定",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        Spacer(),
+      ],
+    ),
+    actions: [
+      Image.asset("images/hoshikawa.png"),
+      const Text(
+        "Judo Timer",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      const SizedBox(width: 80),
+    ],
     elevation: 0,
   );
 
@@ -45,77 +69,6 @@ final backRegion = SizedBox(
     ),
   ),
 );
-
-  // final backRegion = SizedBox(
-  //     child: Column(
-  //         children: [
-  //           Expanded(
-  //             flex: 9,
-  //             child: Container(
-  //               color: const Color.fromARGB(255, 24, 24, 24),
-  //               padding: const EdgeInsets.fromLTRB(30, 5, 30, 30),
-  //               child: Align(
-  //                 child: Container(
-  //                     color: Colors.amber,
-  //                     child: AspectRatio(
-  //                       aspectRatio: 1721 / 940,
-  //                       child: Column(
-  //                         children: [
-  //                           Expanded(
-  //                             flex: 9,
-  //                             child: Align(
-  //                               alignment: Alignment.center,
-  //                               child:Expanded(
-  //                                 child: Row(
-  //                                   children: [
-  //                                     Expanded(flex:4 ,child: Container(color: Colors.red,),),
-  //                                     Expanded(flex:6 ,child: Container(color: Colors.amber,),),
-  //                                     Expanded(flex:4 ,child: Container(color: Colors.red,),),
-  //                                   ],
-  //                                 ),
-  //                                 ),
-  //                             ),
-  //                           ),
-  //                           Expanded(
-  //                             flex: 1,
-  //                             child: Container(color: Colors.green),
-  //                           ),
-  //                           Expanded(
-  //                             flex: 9,
-  //                             child: Row(
-  //                               children: [
-  //                                 Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
-  //                                 Expanded(flex: 1, child: Container(color: Colors.amber,),),
-  //                                 Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
-  //                                 Expanded(flex: 1, child: Container(color: Colors.amber,),),
-  //                                 Expanded(flex: 5, child: Container(color: Colors.blueAccent,),),
-  //                               ],
-  //                             ),
-  //                           ),
-  //                         ],
-  //                       ),
-  //                     ),
-  //                   ),
-  //               ),
-  //             ),
-  //           ),
-  //           Expanded(
-  //               flex: 1,
-  //               child: Container(
-  //                 color: const Color.fromARGB(255, 76, 86, 87),
-  //                 child: Align(
-  //                   alignment: Alignment.center,
-  //                   child: Container(
-  //                     color: Colors.amber,
-  //                     width: 200,
-  //                     child: const Center(child: Text('ボタン')),
-  //                   ),
-  //                 ),
-  //               ),
-  //             ),
-  //         ],
-  //       ),
-  //     );
 
   final scaffold = Scaffold(
     backgroundColor: const Color.fromARGB(255, 24, 24, 24),
