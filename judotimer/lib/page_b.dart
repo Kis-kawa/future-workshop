a1eef03cb725269011b560b55f7b50df3b49bb39
Kyohei Kisikawa <m21041@g.metro-cit.ac.jp>
2024-08-02 02:30:14 +0900

    kis(add): 左の基礎

--- judotimer/lib/page_b.dart
+++ judotimer/lib/page_b.dart
@@ -1,6 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:judotimer/drawer.dart';
 
+class TimeCell {
+  // オートナンバ
+  final String num;
+  // 設定時間
+  final String settime;
+
+  // コンストラクタ
+  TimeCell(this.num, this.settime,);
+}
+
+final models = [
+    TimeCell("1", "10:20"),
+    TimeCell("2", "20:00"),
+    TimeCell("3", "00:20"),
+    TimeCell("4", "00:30"),
+    TimeCell("5", "00:60"),
+    TimeCell("6", "00:20"),
+    TimeCell("7", "00:10"),
+    TimeCell("8", "00:15"),
+];
+
+// モデル => ウィジェット に変換する
+Widget modelToWidget(TimeCell model, BuildContext context) {
+  final double deviceHeight = MediaQuery.of(context).size.height;
+
+  final con = Container(
+    padding: const EdgeInsets.all(1),
+    decoration: const BoxDecoration(
+      border: Border(
+        top: BorderSide(color: Color.fromARGB(255, 118, 118, 118), width: 3.0),
+        right: BorderSide(color: Color.fromARGB(255, 213, 217, 194), width: 10.0),
+        bottom: BorderSide(color: Color.fromARGB(255, 118, 118, 118), width: 3.0),
+        left: BorderSide(color: Color.fromARGB(255, 213, 217, 194), width: 10.0),
+      ),
+      color: Color.fromARGB(255, 202, 202, 202),
+    ),
+    height: ((deviceHeight*0.8)*0.82)*0.17,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+      const Spacer(),
+      Container(
+        width: ((deviceHeight*0.8)*0.82)*0.17*0.65,
+        height: ((deviceHeight*0.8)*0.82)*0.17*0.65,
+        color: Colors.white,
+        child: Center(child: Text(
+                "${model.num}",
+                style: const TextStyle(
+                  fontSize: 24.0,
+                ),
+                textAlign: TextAlign.center,
+              ),),
+      ),
+      const Spacer(),
+      Text("${model.settime}", style: const TextStyle(
+                  fontSize: 24.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+      const Spacer(),
+      const Icon(Icons.restore_from_trash ),
+      const Spacer(),
+    ],),
+  );
+
+  return con;
+}
+
 class PracticeSetting extends StatelessWidget {
   const PracticeSetting({super.key});
 
@@ -43,28 +111,34 @@ class PracticeSetting extends StatelessWidget {
       elevation: 0,
     );
 
+  final list = ListView.builder(
+    itemCount: models.length,
+    itemBuilder: (c, i) => modelToWidget(models[i], c),
+  );
+
     final backRegion = SizedBox(
       width: deviceWidth * 0.9,
       height: deviceHeight * 0.8,
       child: Row(children: [
         Container(
           width: deviceWidth * 0.45,
-          color: Colors.amber,
+          color: Colors.transparent,
           child: Column(
             children: [
               Container(
                 alignment: Alignment.bottomCenter,
                 height: (deviceHeight*0.8)*0.18,
-                color: const Color.fromARGB(255, 205, 87, 87),
-                padding: const EdgeInsets.all(30),
-                child: const Text("data"),
+                color: Colors.transparent,
+                padding: const EdgeInsets.all(5),
+                child: const Text("繰り返し回数 x", style: TextStyle(fontSize: 48.0,),),
               ),
               Container(
-                alignment: Alignment.topCenter,
+                alignment: Alignment.bottomCenter,
                 height: (deviceHeight*0.8)*0.82,
-                color: Colors.pinkAccent,
+                width: (deviceWidth*0.4)*0.7,
+                color: Colors.transparent,
                 padding: const EdgeInsets.all(30),
-                // child: listview,
+                child: list,
               ),
             ],
           ),
